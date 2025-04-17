db2411a487e3f54079c3e7968522018a1b2008d4
SteelSharpensSteel <lionlou0321@gmail.com>
2025-04-17 02:49:59 -0400

    Update test_example.py

--- .github/workflows/test/test_example.py
+++ .github/workflows/test/test_example.py
@@ -1,1 +1,13 @@
-print("This is a test message.")
+print("Running test_example.py")
+
+def test_addition():
+    """A simple test to check addition"""
+    assert 1 + 1 == 2
+
+def test_subtraction():
+    """A simple test to check subtraction"""
+    assert 5 - 3 == 2
+
+def test_example():
+    """Another simple test"""
+    assert 1 + 1 == 2
