82b5d2814ab041394e56f75413c23ad1891dafe2
SteelSharpensSteel <lionlou0321@gmail.com>
2025-04-17 03:12:38 -0400

    Update test_example.py

--- .github/workflows/test/test_example.py
+++ .github/workflows/test/test_example.py
@@ -1,12 +1,9 @@
-# This will confirm the file is being executed
 print("Running test_example.py")
 
-# A simple addition test
 def test_addition():
     """A simple test to check if addition works."""
     assert 1 + 1 == 2
 
-# A simple subtraction test
 def test_subtraction():
     """A simple test to check if subtraction works."""
     assert 5 - 3 == 2
