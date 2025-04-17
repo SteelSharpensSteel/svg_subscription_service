1be920d73bb75b651b1f3741c3d477a9d3d9e7da
SteelSharpensSteel <lionlou0321@gmail.com>
2025-04-16 20:53:11 -0400

    Delete .github/workflows/tests/tests directory

--- .github/workflows/tests/tests/tests_example.py
+++
@@ -1,7 +0,0 @@
-def test_addition():
-    """A simple test to check addition"""
-    assert 1 + 1 == 2
-
-def test_subtraction():
-    """A simple test to check subtraction"""
-    assert 5 - 3 == 2
