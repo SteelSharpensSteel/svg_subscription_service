df2a5b5808d1b51256045785a20e9c5ac7d6ee93
SteelSharpensSteel <lionlou0321@gmail.com>
2025-04-16 21:04:36 -0400

    Create test_example.py

---
+++ .github/workflows/test/test_example.py
@@ -0,0 +1,9 @@
+# This is a simple test file with two test cases
+
+def test_addition():
+    """A simple test to check addition"""
+    assert 1 + 1 == 2
+
+def test_subtraction():
+    """A simple test to check subtraction"""
+    assert 5 - 3 == 2
